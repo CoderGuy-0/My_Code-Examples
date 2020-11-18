2610835f25a58cf811daaf1ebdaf18aed392c30b
CoderGuy-0 <74653470+CoderGuy-0@users.noreply.github.com>
2020-11-18 21:26:07 +0200

    Add files via upload

---
+++ Csharp_app/Csharp_app/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Csharp_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.randomImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.fullScreenF11ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.randomImageToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.fullScreenF11ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // randomImageToolStripMenuItem
+            // 
+            this.randomImageToolStripMenuItem.Name = "randomImageToolStripMenuItem";
+            this.randomImageToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.randomImageToolStripMenuItem.Text = "Open Image...";
+            this.randomImageToolStripMenuItem.Click += new System.EventHandler(this.randomImageToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(115, 112);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(517, 226);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // fullScreenF11ToolStripMenuItem
+            // 
+            this.fullScreenF11ToolStripMenuItem.Name = "fullScreenF11ToolStripMenuItem";
+            this.fullScreenF11ToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
+            this.fullScreenF11ToolStripMenuItem.Text = "FullScreen (F11)";
+            this.fullScreenF11ToolStripMenuItem.Click += new System.EventHandler(this.fullScreenF11ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Picture Viewer";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem randomImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem fullScreenF11ToolStripMenuItem;
+    }
+}
+
---
+++ Csharp_app/Csharp_app/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Program that lets you view your local images
+ */
+
+
+namespace Csharp_app
+{
+    public partial class Form1 : Form
+    {
+        
+
+        
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        // ImageSelector() selects your image file
+        private void ImageSelector()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "*.jpg | *.jpg |*.png | *.png";
+            ofd.FileName = "";
+            
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+              pictureBox2.Image = Image.FromFile(ofd.FileName);
+
+            }
+            if (pictureBox2.Image != null)
+            {
+            pictureBox2.Image.Dispose();
+            pictureBox2.Image = Image.FromFile(ofd.FileName);
+            }
+
+        }
+        // when you click the Open image... button it selects your jpg/png file through ImageSelector() list and shows it in the PictureBox while scaling with the form.
+        private void randomImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox2.Dock = DockStyle.Fill;
+            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+            
+            ImageSelector();
+        }
+
+        // Pressing this button changes form to maximized with basic if/else statements
+        private void fullScreenF11ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+            {
+                WindowState = FormWindowState.Maximized;
+
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+            }
+        }
+
+        // Sets the F11 key to be fullscreen allowing it to maximize and minimize with button press.
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Form1.ActiveForm.KeyPreview = true;
+
+            if (e.KeyCode == Keys.F11)
+            {
+                if (WindowState == FormWindowState.Maximized) 
+                {
+                    WindowState = FormWindowState.Normal;
+                }
+                else
+                {
+                    WindowState = FormWindowState.Maximized; 
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show("Basic image showing app");
+        }
+    }
+    }
+    
+
